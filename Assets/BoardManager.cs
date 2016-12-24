7f4d31c9f30eccc8bd0b8d8c9dba23bae1f90082
kohki nakaji <boomerang.cube@gmail.com>
2016-12-24 18:38:22 +0900

    反転可能な座標を取得するメソッドの追加

--- Assets/BoardManager.cs
+++ Assets/BoardManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Linq;
 using UniRx;
+using System.Collections.Generic;
 
 public class BoardManager : MonoBehaviour
 {
@@ -21,9 +22,13 @@ public class BoardManager : MonoBehaviour
         {
             for (int x = 0; x < 8; ++x)
             {
+                var point = new Point(x, y);    // スコープ的にここで定義
                 Board[y][x].Tapped
-                           .Subscribe(_ => Tapped.OnNext(new Point(x, y)))
-                           .AddTo(gameObject);
+                    .Subscribe(_ =>
+                    {
+                        Tapped.OnNext(point);
+                    })
+                    .AddTo(gameObject);
             }
         }
 
@@ -31,16 +36,88 @@ public class BoardManager : MonoBehaviour
         SetStone(new Action(new Point(3, 4), StoneState.BLACK));
         SetStone(new Action(new Point(4, 3), StoneState.BLACK));
         SetStone(new Action(new Point(4, 4), StoneState.WHITE));
+        SetStone(new Action(new Point(5, 4), StoneState.WHITE));
     }
 
-    public StoneState GetStone(Point p)
+    public StoneState GetStone(Point point)
     {
-        return Board[p.x][p.y].ObservableStoneState.Value;
+        return Board[point.y][point.x].ObservableStoneState.Value;
     }
 
-    public void SetStone(Point p, StoneState state)
+    void SetStone(Action action)
     {
-        Board[p.x][p.y].ObservableStoneState.Value = state;
+        Board[action.point.y][action.point.x].ObservableStoneState.Value = action.state;
+    }
+
+    public bool TrySetStone(Action action)
+    {
+        var points = GetReversiblePoints(action);
+        foreach (var p in points)
+        {
+            Debug.Log("reversible:" + p.x + "," + p.y);
+        }
+
+        if (points.Count > 0)
+        {
+            SetStone(action);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Point> GetReversiblePoints(Action action)
+    {
+        var points = new List<Point>();
+
+        for (int y = -1; y <= 1; ++y)
+        {
+            for (int x = -1; x <= 1; ++x)
+            {
+                // 中心判定しない
+                if (x == 0 && y == 0) { continue; }
+
+                points.AddRange(
+                    GetReversibleDirectionalPoints(action.point, action.state, x, y)
+                );
+            }
+        }
+
+        return points;
+    }
+
+    List<Point> GetReversibleDirectionalPoints(Point p, StoneState selfState, int x_dir, int y_dir)
+    {
+        var i = 0;
+        var points = new List<Point>();
+
+        while (true)
+        {
+            ++i;
+            var nextPoint = new Point(p.x + (x_dir * i), p.y + (y_dir * i));
+            if (!nextPoint.IsValid) { return new List<Point>(); }
+
+            var nextStone = GetStone(nextPoint);
+            if (nextStone.Equals(StoneState.NONE)) { return new List<Point>(); }
+
+            if (nextStone.Equals(selfState))
+            {
+                return points;
+            }
+            points.Add(nextPoint);
+        }
+    }
+
+    StoneState GetOpponent(StoneState state)
+    {
+        switch (state)
+        {
+            case StoneState.BLACK:
+                return StoneState.WHITE;
+            case StoneState.WHITE:
+                return StoneState.BLACK;
+            default:
+                return StoneState.NONE;
+        }
     }
 }
 
@@ -51,7 +128,7 @@ public enum StoneState
     BLACK
 }
 
-public struct Point
+public class Point
 {
     public int x;
     public int y;
@@ -59,5 +136,20 @@ public struct Point
     {
         this.x = x;
         this.y = y;
+    }
+    public bool IsValid
+    {
+        get { return 0 <= x && x < 8 && 0 <= y && y < 8; }
+    }
+}
+
+public struct Action
+{
+    public Point point;
+    public StoneState state;
+    public Action(Point point, StoneState state)
+    {
+        this.point = point;
+        this.state = state;
     }
 }
