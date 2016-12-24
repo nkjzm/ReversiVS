bb5b135b21f1f33707e1f1de56c7e2d3f493f851
kohki nakaji <boomerang.cube@gmail.com>
2016-12-24 19:22:19 +0900

    ゲーム全体の制御を追加

---
+++ Assets/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class GameManager : MonoBehaviour
+{
+    BoardManager board;
+
+    ReactiveProperty<StoneState> currentState = new ReactiveProperty<StoneState>(StoneState.BLACK);
+
+    bool isPassed = false;
+
+    void Start()
+    {
+        board = GetComponentInChildren<BoardManager>();
+
+        board.Tapped
+            .Subscribe(p =>
+            {
+                if (board.TrySetStone(new Action(p, currentState.Value)))
+                {
+                    Debug.Log(string.Format("Set:({0},{1}", p.x, p.y));
+                    currentState.Value = Service.GetOpponent(currentState.Value);
+                }
+            })
+            .AddTo(gameObject);
+
+        currentState
+            .Skip(1)
+            .Subscribe(_ =>
+            {
+                if (CanAvailable())
+                {
+                    isPassed = false;
+                }
+                else
+                {
+                    // 2回連続のパス
+                    if (isPassed)
+                    {
+                        GameSet();
+                        return;
+                    }
+                    isPassed = true;
+                    currentState.Value = Service.GetOpponent(currentState.Value);
+                }
+            })
+            .AddTo(gameObject);
+    }
+
+    void GameSet()
+    {
+        Debug.Log("ゲーム終了");
+    }
+
+    // HACK: 枝切りできる
+    bool CanAvailable()
+    {
+        for (int y = 0; y < 8; ++y)
+        {
+            for (int x = 0; x < 8; ++x)
+            {
+                var count = board.GetReversiblePoints(
+                    new Action(new Point(x, y), currentState.Value)
+                ).Count;
+                if (count > 0)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
